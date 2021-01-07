70065e4cb3038f13fe1657ba3fc4a78e5ac0ec7b
Ryan Bradshaw <ryan@rbradshaw.dev>
2021-01-07 20:43:15 +0000

    started prov

--- files/ztp9300.py
+++ files/ztp9300.py
@@ -1,5 +1,9 @@
 import re
+import urllib2
+import json
 import cli
+
+API_SERVER = "10.0.0.1"
 
 
 def get_model():
@@ -14,24 +18,51 @@ def get_version():
 
 
 def enable_ipxe():
+    print("*** Enabling iPXE to upgrade software ***\n")
     cli.configurep(["boot ipxe timeout 30"])
     cli.executep("copy running-config startup-config")
     cli.executep("reload /noverify")
+
+
+def disable_ipxe():
+    print("*** Disabling iPXE ***\n")
+    cli.configurep(["no boot ipxe timeout 30"])
+    cli.executep("copy running-config startup-config")
+
+
+def check_required(model, version):
+    try:
+        response = urllib2.urlopen("http://%s/api/provisioning/version/%s" % (API_SERVER, model))
+        data = json.loads(response.read().decode("utf-8"))
+    except urllib2.HTTPError as e:
+        print(e.reason)
+        return None
+    except urllib2.URLError as e:
+        print(e.reason)
+        return None
+    else:
+        return data if version != data["version"] else None
 
 
 def main():
     # Get model
     model = get_model()
     if model:
-        print("Model is: %s" % model)
+        print("\nModel is: %s" % model)
     # Check version
     version = get_version()
     if version:
-        print("Version is: %s" % version)
+        print("\nVersion is: %s" % version)
     # Check upgrade
-
+    upgrade_required = check_required(model, version)
+    if upgrade_required:
+        print("Current software version: %s" % version)
+        print("Required software version: %s" % upgrade_required["version"])
+        enable_ipxe()
     # Remove iPXE
-
+    else:
+        print("Software upgrade not required")
+        disable_ipxe()
     # Request config
 
 
